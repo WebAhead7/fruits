f4bee2458138c79587a56342a800405bcf586cad
OmklthomAmara <amaraomklthom@gmail.com>
2020-11-04 13:27:35 +0200

    dotenv file

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const router = require("./router");
 require("dotenv").config();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const server = http.createServer(router);
 
 server.listen(PORT, () => console.log(`we're online at http://localhost:3000`));
